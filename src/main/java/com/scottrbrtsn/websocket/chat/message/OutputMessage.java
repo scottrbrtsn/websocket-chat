76277e8fa4ca51e1e11a469b56b027c921f102b1
Scott Robertson <scott.robertson@saic.com>
2019-02-06 15:13:00 -0600

    deleted code in the wrong place

--- src/main/java/com/scottrbrtsn/websocket/chat/message/OutputMessage.java
+++ src/main/java/com/scottrbrtsn/websocket/chat/message/OutputMessage.java
@@ -2,11 +2,6 @@ package com.scottrbrtsn.websocket.chat.message;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -15,12 +10,5 @@ public class OutputMessage {
     private String from;
     private String content;
     private String time;
-
-    @MessageMapping("/chat")
-    @SendTo("/topic/messages")
-    public OutputMessage send(Message message) throws Exception {
-        String time = new SimpleDateFormat("HH:mm").format(new Date());
-        return new OutputMessage(message.getFrom(), message.getContent(), time);
-    }
 
 }
